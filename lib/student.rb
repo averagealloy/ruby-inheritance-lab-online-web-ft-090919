63f8dbd97b41ec207da554c74727d58cfae70860
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 00:35:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
-class Student
-  class Student < User
+class Student 
+class Student < User
 
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
   end
-end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
