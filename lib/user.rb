2373ecc0ed4b90b8d7eaea9df393ed91878a8e64
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 00:26:02 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,11 @@
 class User
 attr_accessor :user
 
-#def initialize(first_name, last_name)
-#@first_name = first_name
-#@last_name = lastname
+def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = lastname
 
-#end
+end
 
 
 end
