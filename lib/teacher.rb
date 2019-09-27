01b3f2b907b237bb6739036186ca8e5d0ae59368
Mike McTighe <m.mctighe10@gmail.com>
2019-09-27 00:42:18 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,5 +1,4 @@
-class Teacher
-  class Teacher < User
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
@@ -7,4 +6,3 @@ class Teacher
     KNOWLEDGE.sample
   end
 end
-end 
